190ff1f6929cf579eec3ddbe6f9798a4958c5f32
dtechbro <princeakinola05@gmail.com>
2025-01-08 22:20:02 +0100

    waiting for review

--- app/speakers/page.jsx
+++ app/speakers/page.jsx
@@ -5,18 +5,39 @@ import { speakerList } from "../components/Speaker";
 import Image from "next/image";
 import Footer from "../components/Footer";
 
+function truncateBio(bio, wordCount) {
+  return (
+    bio.split(" ").slice(0, wordCount).join(" ") +
+    (bio.split(" ").length > wordCount ? "..." : "")
+  );
+}
+
 export default function Speakers() {
   return (
     <main>
       <Navbar />
 
       <section
-        className="flex flex-col gap-10 py-5 md:px-20 md:py-48"
+        className="flex flex-col gap-14 max-w-7xl mx-auto p-5 my-20 lg:my-28"
         id="speakers"
       >
+        <div className="max-w-4xl mx-auto flex flex-col items-center">
+          <h2 className="text-dark mt-2.5 text-lg md:text-3xl font-semibold">
+            Speaker Lineup
+          </h2>
+          <p className="text-center text-sm md:text-lg mt-1.5">
+            We&apos;re bringing together some of the most audacious thinkers and
+            doers in African tech, working on everuthing from fintech to
+            commerce, energy, Web3 and AI, telco, cloud and more.
+          </p>
+        </div>
+
         {speakerList.map((speaker) => (
-          <div key={speaker.id} className="border rounded-xl flex min-h-40">
-            <div className="h-full w-4/12 relative">
+          <div
+            key={speaker.id}
+            className="border rounded-xl flex flex-col gap-3 lg:flex-row lg:items-center min-h-80 overflow-hidden"
+          >
+            <div className="h-80 lg:w-4/12 relative">
               <Image
                 src={speaker.profile}
                 alt={`Profile of ${speaker.name}`}
@@ -25,10 +46,12 @@ export default function Speakers() {
               />
             </div>
 
-            <div className="w-8/12">
-              <h2>{speaker.name}</h2>
-              <p>{speaker.position}</p>
-              <p>{speaker.bio}</p>
+            <div className="lg:w-8/12 px-2 lg:px-5">
+              <h2 className="font-semibold text-lg">{speaker.name}</h2>
+              <p className="text-sm text-gray-500">{speaker.position}</p>
+              <p className="text-clip mt-5 text-sm">
+                {truncateBio(speaker.bio, 100)}
+              </p>
             </div>
           </div>
         ))}
